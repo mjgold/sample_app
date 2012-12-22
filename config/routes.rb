273e3226df4c25ae745649d35ad70f0af5ff8a2a
Mark Goldenson <mjgold1@gmail.com>
2012-12-22 15:06:51 -0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 SampleApp::Application.routes.draw do
   
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   get "users/new"
 
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/',        to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,7 @@ describe User do
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
+    it { should respond_to(:remember_token) }
     it { should respond_to(:authenticate) }
 
     it { should be_valid }
@@ -43,7 +44,6 @@ describe User do
       	before { @user.name = "a" * 51 }
     	it { should_not be_valid }
 	end
-
   
 	describe "when email format is invalid" do
     	it "should be invalid" do
@@ -111,4 +111,9 @@ describe User do
   		before { @user.password = @user.password_confirmation = "a" * 5 }
   		it { should be_invalid }
 	end
+
+	describe "remember token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
+    end
 end
